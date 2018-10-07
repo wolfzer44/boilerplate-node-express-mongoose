10ce4bbc4cec7c58f6f328ac4fbbb195fb66dc31
Matheus Oliveira <mattoliveirabsb@gmail.com>
2018-10-07 12:54:06 -0600

    refactory routes

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,13 @@
 const routes = require('express').Router();
+const ctrl = require('../controllers');
 
+routes
+    .get('/api', ctrl.api.get)
+    .post('/api', ctrl.api.post);
 
-routes.get('/api', (req, res) => {
-    res.json({
-        router: '/api',
-        method: 'GET'
-    })
-});
-
-routes.get('/go', (req, res) => {
-    res.json({
-        router: '/go',
-        method: 'GET'
-    })
-});
+routes
+    .get('/go', ctrl.go.get)
+    .post('/go', ctrl.go.post);    
 
 
 
