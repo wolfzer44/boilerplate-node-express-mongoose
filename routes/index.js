44a70db833a03f8e4453f18ea3b1796e0c59e26a
Matheus Oliveira <mattoliveirabsb@gmail.com>
2018-10-07 12:32:51 -0600

    refactory routes file

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
 const routes = require('express').Router();
 
+
 routes.get('/api', (req, res) => {
     res.json({
         router: '/api',
         method: 'GET'
     })
 });
+
+routes.get('/go', (req, res) => {
+    res.json({
+        router: '/go',
+        method: 'GET'
+    })
+});
+
+
 
 module.exports = routes;
