0a3da841de654d679c50162410a1945c59d7e3f8
Matheus Oliveira <mattoliveirabsb@gmail.com>
2018-10-07 03:45:28 -0600

    New features add

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const app = require('express')(); 
 const dotenv = require('dotenv');
+const requireDir = require('require-dir');
 
 dotenv.config();
+const config = requireDir('./config');
 
+app.get('/', (req, res)=> {
+    res.json({
+        message: `Server running on ${process.env.PORT}`
+    })
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on: ${process.env.PORT}`);
